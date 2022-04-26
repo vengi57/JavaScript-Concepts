3b7315132a7c62099fa5a85358d598dc3b1368a6
Vengi Thiyagarajan <32476102+vengi57@users.noreply.github.com>
2022-04-26 23:01:01 +0530

    Update hosting.js
    Added example for hosting

--- Firstclassfunctions/hosting.js
+++ Firstclassfunctions/hosting.js
@@ -10,3 +10,22 @@ function y() {
 var z = function () {
     console.log("Assignment function");
 }
+
+
+//Hosting 
+/**
+ * Hosting only moves the variable declaration not the assignment
+ * Be aware of console logs (See the example at line 12 and 17)
+ */
+
+x = 1
+if (x === 1) {
+  x = 2;
+  console.log(y)
+  y = 4
+  console.log("x1",x);
+  var y;
+  console.log(y)
+}
+console.log("x2",x);
+var x ;
